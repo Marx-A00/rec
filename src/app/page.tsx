0cb7d300bd5af329ba3335c129becdcbd7f3d6c4
Marcos Andrade <mnandrade1999@gmail.com>
2025-01-09 21:02:44 -0600

    wip: test page for layout and more in album type interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
 import Image from "next/image";
+
+interface Album {
+  id: string;
+  title: string;
+  artist: string;
+  image: string;
+  score: number;
+}
+
+const album1: Album = {
+  id: "1",
+  title: "Album 1",
+  artist: "Artist 1",
+  image: "/image1.png",
+  score: 90,
+};
+
+const album2: Album = {
+  id: "2",
+  title: "Album 2",
+  artist: "Artist 2",
+  image: "/image2.png",
+  score: 80,
+};
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="flex flex-col gap-4 items-center">
         <h1 className="text-2xl font-bold text-center">Test Data Display</h1>
         <div className="flex gap-4">
+          <div>
+            {album1.title}
+          </div>
+            {album1.score}
+          <div>
+            {album2.title}
+          </div>
+            {album2.score}
           <Image
             src="/image1.png"
             alt="Image 1"
