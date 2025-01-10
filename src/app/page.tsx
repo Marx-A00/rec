b6e139e6cc2464b6a8fe27e0a8dc08a0c3367bfa
Marcos Andrade <mnandrade1999@gmail.com>
2025-01-10 12:37:21 -0600

    wip: changed home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-
-
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-5xl font-bold">If u dont know by now, I'm talkin bout Chi town</h1>
+      <div className="flex flex-row space-x-4">
+        <button className="text-white bg-red-500 hover:bg-red-700 font-bold py-4 px-8 rounded-full text-lg shadow-lg">
+          My Profile
+        </button>
+        <button className="text-white bg-red-500 hover:bg-red-700 font-bold py-4 px-8 rounded-full text-lg shadow-lg">
+          Button 2
+        </button>
+        <button className="text-white bg-red-500 hover:bg-red-700 font-bold py-4 px-8 rounded-full text-lg shadow-lg">
+          Recommend
+        </button>
+        <button className="text-white bg-red-500 hover:bg-red-700 font-bold py-4 px-8 rounded-full text-lg shadow-lg">
+          Button 4
+        </button>
+        <button className="text-white bg-red-500 hover:bg-red-700 font-bold py-4 px-8 rounded-full text-lg shadow-lg">
+          Search
+        </button>
+      </div>
     </div>
   );
 }
