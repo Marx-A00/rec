da76bf304b855fab11196552886a525d35ab3354
Marcos Andrade <mnandrade1999@gmail.com>
2025-01-13 21:36:32 -0600

    new profile component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex flex-row space-x-4">
-        <button className="text-white bg-red-500 hover:bg-red-700 font-bold py-4 px-8 rounded-full text-lg shadow-lg">
-          My Profile
-        </button>
+        <Link href="/profile"
+        className="text-white bg-red-500 hover:bg-red-700 font-bold py-4 px-8 rounded-full text-lg shadow-lg"
+        >My Profile</Link>
         <button className="text-white bg-red-500 hover:bg-red-700 font-bold py-4 px-8 rounded-full text-lg shadow-lg">
           Button 2
         </button>
