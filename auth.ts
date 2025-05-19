2856997f8dd78ecbd8797db380608cd3f4419b68
Marcos Andrade <marcos.andrade.dev@gmail.com>
2025-05-18 22:27:45 -0500

    wip: working on search endpoint

--- auth.ts
+++ auth.ts
@@ -4,18 +4,25 @@ import Google from "next-auth/providers/google"
 import Spotify from "next-auth/providers/spotify"
 import prisma from "@/lib/prisma"
 
+console.log("Loaded ENV variables:");
+console.log({
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+});
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
+
     providers: [
+
         Google,
         Spotify({
-            clientId: process.env.SPOTIFY_CLIENT_ID ?? "",
-            clientSecret: process.env.SPOTIFY_CLIENT_SECRET ?? "",
-            authorization: {
-                params: {
-                    scope: 'user-read-email user-top-read user-read-recently-played'
-                }
-            }
+            clientId: process.env.SPOTIFY_CLIENT_ID!,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+            authorization: "https://accounts.spotify.com/authorize?scope=user-read-email user-top-read user-read-recently-played"
         })
     ],
 })
